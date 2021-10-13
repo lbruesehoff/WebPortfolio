44fefb32921fee6a5442a3727ad11c8c6d0b13fa
lbruesehoff <lbruesehoff@gmail.com>
2021-10-13 01:44:53 -0500

    Footer for Desktop

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import './Footer.css';
+import logo from './logo.svg'
+import gitHub from './github.svg'
+import linkedIn from './linkedin.svg'
 
 export default class Footer extends Component {
   render() {
     return (
       <div className="footerContent">
 
-        <footer>
+        
           <ul>
+            <li>
+              <div className="footerLogo">
+                <img src={logo} />
+              </div>
+            </li>
             <li>Home</li>
             <li>Portfolio</li>
             <li>Contact</li>
+            <li>
+              <div className="gitHub">
+                <img src={gitHub}/>
+              </div>
+            </li>
+            <li>
+              <div className="linkedIn">
+              <img src={linkedIn}/>
+              </div>
+            </li>
           </ul>
-        </footer>
+        
         
       </div>
     )
