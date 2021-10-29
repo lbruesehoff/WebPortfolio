2686e18e9ad65dc7bf36a20578bb40b63385b36b
lbruesehoff <lbruesehoff@gmail.com>
2021-10-29 14:01:53 -0500

    Navbar collapse

--- src/components/Navbar/NavbarComp.js
+++ src/components/Navbar/NavbarComp.js
@@ -17,9 +17,9 @@ export default class NavbarComp extends Component {
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ms-auto">
-                  <Link to="/">Home</Link>
-                  <Link to="../../Portfolio">Portfolio</Link>
-                  <Link to="../../Contact">Contact</Link>
+                  <Nav.Link eventKey="1" as={Link} to="/">Home</Nav.Link>
+                  <Nav.Link eventKey="2" as={Link} to="../../Portfolio">Portfolio</Nav.Link>
+                  <Nav.Link eventKey="3" as={Link} to="../../Contact">Contact</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
               </Container>
