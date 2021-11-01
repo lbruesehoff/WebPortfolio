40b007d2692a262f8a5c42bad2a602f5e42149ee
lbruesehoff <lbruesehoff@gmail.com>
2021-10-31 19:04:49 -0500

    .htaccess

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import NavbarComp from "./components/Navbar/NavbarComp";
 import Footer from './components/Footer/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from './Home';
 import Portfolio from './Portfolio';
 import Contact from './Contact';
